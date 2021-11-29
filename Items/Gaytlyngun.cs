0f31df3619468bcf23c3df6a181d8f11da9b5d46
wyvernbw <iamwyvern511@gmail.com>
2021-11-29 18:55:15 +0200

    Fix dashing

--- Items/Gaytlyngun.cs
+++ Items/Gaytlyngun.cs
@@ -36,20 +36,54 @@ namespace springEquinox.Items
             recipe.Register();
         }
 
+	    DashPlayer mp; 
         public override void MeleeEffects(Player player, Rectangle hitbox)
         {
-            public int timer=0;
-
-            player.velocity.X += 4.0f * player.direction;
-            while(timer<60)
-            {
-              ++timer;
-            }
-            player.velocity.X = 0f;
-
-
+            mp.DashingActive = true;
+            mp.OldVelocity = player.velocity;
+            //player.velocity.X += 4.0f * player.direction;
         }
 
-    }
+        public override void UpdateInventory(Player player) 
+        {
+            mp = player.GetModPlayer<DashPlayer>();
 
+
+            if (!mp.DashingActive) return;
+
+            player.eocDash = mp.DashTimer;
+			player.armorEffectDrawShadowEOCShield = true;
+
+            // player.velocity.X += mp.DashVelocity * player.direction;
+            if(mp.DashTimer == DashPlayer.MAX_DASH_TIMER)
+			{	
+				player.velocity.X += mp.DashVelocity * player.direction;
+			}
+
+			//Decrement the timers
+			mp.DashTimer--;
+			mp.DashDelay--;
+            if(mp.DashDelay == 0)
+			{
+				//The dash has ended.  Reset the fields
+				mp.DashDelay = DashPlayer.MAX_DASH_DELAY;
+				mp.DashTimer = DashPlayer.MAX_DASH_TIMER;
+				mp.DashingActive = false;
+                player.velocity /= 10.0f;
+			}
+        }
+
+        public class DashPlayer : ModPlayer
+        { 
+            public bool DashingActive = false;
+            public int DashDelay = MAX_DASH_DELAY;
+            public int DashTimer = MAX_DASH_TIMER;
+            public readonly float DashVelocity = 10.0f;
+            public Vector2 OldVelocity;
+            //These two fields are the max values for the delay between dashes and the length of the dash in that order
+            //The time is measured in frames
+            public static readonly int MAX_DASH_DELAY = 25;
+            public static readonly int MAX_DASH_TIMER = 35;
+        }
+    }
 }
